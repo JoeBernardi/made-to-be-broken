498fc5df938f0f932464697f46db09c3c7859592
Joe Bernardi <gloveboxjoe@gmail.com>
2019-08-21 12:02:10 -0400

    Add mobile detector/don't even show stuff for mobile devices

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Express = require('express')
 const bodyParser = require('body-parser')
 const browserify = require('browserify-middleware')
 const sassMiddleware = require('node-sass-middleware')
+const MobileDetect = require('mobile-detect')
 
 const getVideos = require('./videos')
 
@@ -22,7 +23,6 @@ app.use(
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(Express.static('templates'))
 app.use(Express.static('assets'))
 
 app.post('/', function (req, res) {
@@ -33,7 +33,12 @@ app.post('/', function (req, res) {
 })
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html')
+  const md = new MobileDetect(req.headers['user-agent']);
+  if (md.mobile()) {
+    res.sendFile('/templates/index-mobile.html', { root: __dirname });
+  } else {
+    res.sendFile('/templates/index.html', { root: __dirname })
+  }
 })
 
 app.listen(8081, () => console.log('🐚  🐚  🐚   S E R V E R  U P  🐚  🐚  🐚'))
